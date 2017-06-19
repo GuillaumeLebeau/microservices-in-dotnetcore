56e6e8eb59265f9746de6d97824e28ebfded25a6
Guillaume LEBEAU <guillaume.lebeau@irec.fr>
2017-06-19 15:52:47 +0200

    2.2.5 Add failure-handling policy

--- chapter2/ShoppingCart/ProductCatalog/ProductCatalogClient.cs
+++ chapter2/ShoppingCart/ProductCatalog/ProductCatalogClient.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Polly;
 using ShoppingCart.ShoppingCart;
 
 namespace ShoppingCart.ProductCatalog
 {
     public class ProductCatalogClient : IProductCatalogClient
     {
+        // URL for the fake Product Catalog microservice
         private static string productCatalogBaseUrl = @"http://private-05cc8-chapter2productcataloguemicroservice.apiary-mock.com";
+
         private static string getProductPathTemplate = "/products?productIds=[{0}]";
 
-        public Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItems(int[] productCatalogIds)
-        {
-            throw new NotImplementedException();
-        }
+        // Use Polly's fluent API to set up a retry policy with an exponential back-off
+        private static Policy exponentialRetryPolicy = Policy.Handle<Exception>()
+            .WaitAndRetryAsync(3, attempt => TimeSpan.FromMilliseconds(100 * Math.Pow(2, attempt)));
+
+        public Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItems(int[] productCatalogIds) =>
+            // Wraps calls to the Product Catalog microservice in the retry policy
+            exponentialRetryPolicy.ExecuteAsync(async () => await GetItemsFromCatalogService(productCatalogIds).ConfigureAwait(false));
 
         private async Task<IEnumerable<ShoppingCartItem>> GetItemsFromCatalogService(int[] productCatalogIds)
         {
@@ -26,10 +32,15 @@ namespace ShoppingCart.ProductCatalog
 
         private static async Task<HttpResponseMessage> RequestProductFromProductCatalog(int[] productCatalogIds)
         {
+            // Adds the product IDs as a query string parameter to the path of the /products endpoint
             var productsResource = string.Format(getProductPathTemplate, string.Join(",", productCatalogIds));
+
+            // Creates a client for making the HTTP GET request
             using (var httpClient = new HttpClient())
             {
                 httpClient.BaseAddress = new Uri(productCatalogBaseUrl);
+
+                // Tells HttpClient to perform HTTP GET asynchronously
                 return await httpClient.GetAsync(productsResource).ConfigureAwait(false);
             }
         }
@@ -37,10 +48,15 @@ namespace ShoppingCart.ProductCatalog
         private static async Task<IEnumerable<ShoppingCartItem>> ConvertToShoppingCartItems(HttpResponseMessage response)
         {
             response.EnsureSuccessStatusCode();
+
+            // Uses Json.NET to deserialize the JSON from the Product Catalog microservice
             var products = JsonConvert.DeserializeObject<List<ProductCatalogProduct>>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+
+            // Creates a ShoppingCartItem for each product in the response
             return products.Select( p => new ShoppingCartItem(int.Parse(p.ProductId), p.ProductName, p.ProductDescription, p.Price));
         }
 
+        // Use a private class to represent the product data
         private class ProductCatalogProduct
         {
             public string ProductId { get; set; }
