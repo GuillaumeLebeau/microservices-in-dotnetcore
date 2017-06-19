9e3346745f2ef4b91d9a216f3f9b98a5c293f05e
Guillaume LEBEAU <guillaume.lebeau@irec.fr>
2017-06-19 14:52:47 +0200

    2.2.2 Shopping Cart API

---
+++ chapter2/ShoppingCart/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using ShoppingCart.EventFeed;
+
+namespace ShoppingCart.ShoppingCart
+{
+    public class ShoppingCart
+    {
+        private HashSet<ShoppingCartItem> items = new HashSet<ShoppingCartItem>();
+
+        public ShoppingCart(int userId)
+        {
+            this.UserId = userId;
+        }
+
+        public int UserId { get; }
+
+        public IEnumerable<ShoppingCartItem> Items
+        {
+            get { return items; }
+        }
+
+        public void AddItems(IEnumerable<ShoppingCartItem> shoppingCartItems, IEventStore eventStore)
+        {
+            foreach (var item in shoppingCartItems)
+            {
+                if (this.items.Add(item))
+                    eventStore.Raise("ShoppingCartItemAdded", new { UserId, item });
+            }
+        }
+
+        public void RemoveItems(int[] productCatalogueIds, IEventStore eventStore)
+        {
+            items.RemoveWhere(i => productCatalogueIds.Contains(i.ProductCatalogueId));
+        }
+    }
+}
