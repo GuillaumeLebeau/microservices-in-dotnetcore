da92a641220b85ff33553769a646bea994737cf5
Guillaume LEBEAU <guillaume.lebeau@irec.fr>
2017-06-19 15:32:11 +0200

    2.2.4 Parsing product response

--- chapter2/ShoppingCart/ProductCatalog/ProductCatalogClient.cs
+++ chapter2/ShoppingCart/ProductCatalog/ProductCatalogClient.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using ShoppingCart.ShoppingCart;
 
 namespace ShoppingCart.ProductCatalog
@@ -11,19 +13,40 @@ namespace ShoppingCart.ProductCatalog
         private static string productCatalogBaseUrl = @"http://private-05cc8-chapter2productcataloguemicroservice.apiary-mock.com";
         private static string getProductPathTemplate = "/products?productIds=[{0}]";
 
-        public Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItems(int[] productCatalogueIds)
+        public Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItems(int[] productCatalogIds)
         {
             throw new NotImplementedException();
         }
 
-        private static async Task<HttpResponseMessage> RequestProductFromProductCatalogue(int[] productCatalogueIds)
+        private async Task<IEnumerable<ShoppingCartItem>> GetItemsFromCatalogService(int[] productCatalogIds)
         {
-            var productsResource = string.Format(getProductPathTemplate, string.Join(",", productCatalogueIds));
+            var response = await RequestProductFromProductCatalog(productCatalogIds).ConfigureAwait(false);
+            return await ConvertToShoppingCartItems(response).ConfigureAwait(false);
+        }
+
+        private static async Task<HttpResponseMessage> RequestProductFromProductCatalog(int[] productCatalogIds)
+        {
+            var productsResource = string.Format(getProductPathTemplate, string.Join(",", productCatalogIds));
             using (var httpClient = new HttpClient())
             {
                 httpClient.BaseAddress = new Uri(productCatalogBaseUrl);
                 return await httpClient.GetAsync(productsResource).ConfigureAwait(false);
             }
+        }
+
+        private static async Task<IEnumerable<ShoppingCartItem>> ConvertToShoppingCartItems(HttpResponseMessage response)
+        {
+            response.EnsureSuccessStatusCode();
+            var products = JsonConvert.DeserializeObject<List<ProductCatalogProduct>>(await response.Content.ReadAsStringAsync().ConfigureAwait(false));
+            return products.Select( p => new ShoppingCartItem(int.Parse(p.ProductId), p.ProductName, p.ProductDescription, p.Price));
+        }
+
+        private class ProductCatalogProduct
+        {
+            public string ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string ProductDescription { get; set; }
+            public Money Price { get; set; }
         }
     }
 }
