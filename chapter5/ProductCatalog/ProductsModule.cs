9347208c4b132513a3561da1242621779b76ee22
Guillaume Lebeau <glebeau.dev@gmail.com>
2017-07-05 18:23:02 +0200

    5.3.3 Setting cache headers in Nancy responses

---
+++ chapter5/ProductCatalog/ProductsModule.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Nancy;
+
+namespace ProductCatalog
+{
+    public class ProductsModule : NancyModule
+    {
+        public ProductsModule(IProductStore productStore)
+            : base("/products")
+        {
+            Get("/", _ =>
+            {
+                string productIdsString = Request.Query.productIds;
+                var productIds = ParseProductIdsFromQueryString(productIdsString);
+                var products = productStore.GetProductsByIds(productIds);
+
+                return Negotiate
+                    .WithModel(products)
+                    .WithHeader("cache-control", "max-age:86400");
+            });
+        }
+
+        private static IEnumerable<int> ParseProductIdsFromQueryString(string productIdsString)
+        {
+            return productIdsString.Split(',').Select(s => s.Replace("[", "").Replace("]", "")).Select(int.Parse);
+        }
+    }
+
+    public interface IProductStore
+    {
+        IEnumerable<ProductCatalogProduct> GetProductsByIds(IEnumerable<int> productIds);
+    }
+
+    public class StaticProductStore : IProductStore
+    {
+        public IEnumerable<ProductCatalogProduct> GetProductsByIds(IEnumerable<int> productIds)
+        {
+            return productIds.Select(id => new ProductCatalogProduct(id, "foo" + id.ToString(), "bar", new Money()));
+        }
+    }
+
+    public class ProductCatalogProduct
+    {
+        public ProductCatalogProduct(int productId, string productName, string description, Money price)
+        {
+            ProductId = productId.ToString();
+            ProductName = productName;
+            ProductDescription = description;
+            Price = price;
+        }
+
+        public string ProductId { get; }
+        public string ProductName { get; }
+        public string ProductDescription { get; }
+        public Money Price { get; }
+    }
+
+    public class Money
+    {
+    }
+}
